d806bcf9c2735123d2fd043fad443123cd1d3977
ewagurga <ewagurga@gmail.com>
2023-04-27 23:05:40 +0200

    Add project files.

---
+++ ElementPresent/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ElementPresent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElementPresent")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7546ff8f-8579-4ca3-bc50-9df49a3c4846")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ElementPresent/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using OpenQA.Selenium.DevTools.V110.Emulation;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Configuration;
+
+namespace FPTests
+{
+    [TestClass]
+    public class FPTests
+    {
+        [TestMethod]
+        public void TestDesktopResolution()
+
+        {
+            IWebDriver driver = new ChromeDriver();
+            string url = "https://us.balmain.com/en/p/neoprene-and-leather-unicorn-low-top-sneakers-AM1VJ309KNLR0FA.html";
+            driver.Navigate().GoToUrl(url);
+
+            driver.Manage().Window.Maximize();
+
+            IWebElement dismissButton = driver.FindElement(By.XPath("/html/body/div[7]/div/div/form/button[2]"));
+            dismissButton.Click();
+
+            IWebElement fitPredictor = driver.FindElement(By.ClassName("fp-element"));
+            Assert.AreEqual(true, fitPredictor.Displayed);
+
+            Thread.Sleep(3000);
+            
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMobileResolution()
+        {
+            IWebDriver driver = new ChromeDriver();
+            string url = "https://us.balmain.com/en/p/neoprene-and-leather-unicorn-low-top-sneakers-AM1VJ309KNLR0FA.html";
+            driver.Navigate().GoToUrl(url);
+
+            Console.WriteLine(driver.Manage().Window.Size);
+
+
+            var deviceModeSetting = new SetDeviceMetricsOverrideCommandSettings();
+            deviceModeSetting.Width = 600;
+            deviceModeSetting.Height = 1000;
+            deviceModeSetting.Mobile = true;
+            deviceModeSetting.DeviceScaleFactor = 50;
+
+            IWebElement dismissButton = driver.FindElement(By.XPath("/html/body/div[7]/div/div/form/button[2]"));
+            dismissButton.Click();
+
+            IWebElement fitPredictor = driver.FindElement(By.ClassName("fp-element"));
+            Assert.AreEqual(true, fitPredictor.Displayed);
+
+            Thread.Sleep(3000);
+
+           
+        }
+        
+        [TestMethod]
+        public void AddCalculatorSeed()
+
+        {
+            IWebDriver driver = new ChromeDriver();
+            string url = "https://us.balmain.com/en/p/neoprene-and-leather-unicorn-low-top-sneakers-AM1VJ309KNLR0FA.html";
+            driver.Navigate().GoToUrl(url);
+
+            IWebElement dismissButton = driver.FindElement(By.XPath("/html/body/div[7]/div/div/form/button[2]"));
+            dismissButton.Click();
+
+            IWebElement fitPredictor = driver.FindElement(By.ClassName("fp-element"));
+            fitPredictor.Click();
+
+            IWebElement shoeBrand = driver.FindElement(By.CssSelector("#js-attribute > div.fp-root.fp-site-language-en > div.fp-backdrop-container > div > div > div > div > div.fp-wizard-application > div > div.fp-inner > div.fp-calculator-form > div.fp-dropdown-container.fp-brand > select"));
+            Thread.Sleep(2000);
+            shoeBrand.Click();
+
+          
+
+        }
+
+        
+       
+
+
+ 
+
+    }
+    }
+
